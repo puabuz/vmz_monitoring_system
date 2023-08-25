e58361b8aa49a56626e8297f93c0328c2658375d
Семёнов Виталий Анатольевич <vitaly_s@vmz.local>
2023-08-25 09:39:45 +0300

    fix-2

--- src/axios.js
+++ src/axios.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 
+const ENV = process.env.NODE_ENV === "production" ? "https://cloud2" : "http://192.168.105.167:5000/"
+
 //каждый URL адрес, который мы запрашиваем, будет добавлять этот префикс
 // обязательно проимпортировать в main.js
 // axios.defaults.baseURL = "http://cloud/";  // http://192.168.105.167:8080/#/login  /
-// axios.defaults.baseURL = "https://cloud/"
+// axios.defaults.baseURL = "https://cloud2/"
+axios.defaults.baseURL = ENV;
 // axios.defaults.baseURL = "http://localhost:5000/"; 
-axios.defaults.baseURL = "http://192.168.105.167:5000/";
 // axios.defaults.baseURL = "http://192.168.105.12:8000/";
 axios.defaults.withCredentials = true;
 
